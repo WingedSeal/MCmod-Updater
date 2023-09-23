c68b80b8e27f46da23795d77015bada4efc4aaaf
WingedSeal <firm09719@gmail.com>
2023-09-23 17:07:35 +0700

    fix: fix neu crashing

--- src/mcmu.py
+++ src/mcmu.py
@@ -149,7 +149,7 @@ def get_custom_urls(lines: list[str]) -> list[str]:
             for _mod in response.json():
                 if _mod["id"] != "neu":
                     continue
-                urls.append(urls["url"])
+                urls.append(_mod["url"])
 
     return urls
 
